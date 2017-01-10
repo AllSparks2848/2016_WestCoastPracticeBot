81e1e7591087a6641391ac02c07517359b7ac15a
WillCurran <willecurran@gmail.com>
2017-01-10 00:17:52 -0600

    V1.0
    Added Shooter functionality.  Up for testing: Driving --> Shooter -->
    Shooter PID --> printing/graphing velocity

--- src/org/usfirst/frc/team2850/robot/Robot.java
+++ src/org/usfirst/frc/team2850/robot/Robot.java
@@ -20,6 +20,8 @@ public class Robot extends IterativeRobot {
     public static Joystick xbox1;
 	
 	public static RobotDrive drivetrain;
+	public static RobotDrive drivetrain2;
+	
 	public static Spark leftDrive1;
 	public static Spark rightDrive1;
 	public static Spark leftDrive2;
@@ -28,22 +30,26 @@ public class Robot extends IterativeRobot {
 	public static Spark rightDrive3;
 	public static Compressor compressor;
 	public static Solenoid driveshifter;
-	public static Encoder leftEncoder;
-	public static Encoder rightEncoder;
+//	public static Encoder leftEncoder;
+//	public static Encoder rightEncoder;
+	
+	//public static Spark shooterMotor;                //added V1.0
+	//public static Encoder shooterEncoder;
+	//public static Shooter shooter;
 	
     public static boolean high;
     //how many pulses per rotation? how many feet per rotation? (gearing, tires) needs to be decided
-    public static double DIST_PER_PULSE = .2;
-   	
-    public static double Kp = .005; //proportional gain
-    public static double Ki = .005; //integral gain
-    public static double Kd = .005; //derivative gain
-    
-    public static double PID_SETPOINT = 10.0; //10 feet
-    public static double TARGET_THRESHOLD = .25; //within .25 ft is ok
-    
-   	public static PID linearDriveLeft = new PID(Kp, Ki, Kd, PID_SETPOINT, leftEncoder.getDistance());
-   	public static PID linearDriveRight = new PID(Kp, Ki, Kd, PID_SETPOINT, rightEncoder.getDistance());
+//    public static double DIST_PER_PULSE = .2;
+//   	
+//    public static double Kp = .005; //proportional gain
+//    public static double Ki = .005; //integral gain
+//    public static double Kd = .005; //derivative gain
+//    
+//    public static double PID_SETPOINT = 10.0; //10 feet
+//    public static double TARGET_THRESHOLD = .25; //within .25 ft is ok
+//    
+//   	public static PID linearDriveLeft = new PID(Kp, Ki, Kd, PID_SETPOINT, leftEncoder.getDistance());
+//   	public static PID linearDriveRight = new PID(Kp, Ki, Kd, PID_SETPOINT, rightEncoder.getDistance());
    	
     /**
      * This function is run when the robot is first started up and should be
@@ -53,77 +59,67 @@ public class Robot extends IterativeRobot {
     	xbox1=new Joystick(0);
     	leftDrive1=new Spark(0);
     	leftDrive2=new Spark(1);
-    	rightDrive1=new Spark(2);
-    	rightDrive2=new Spark(3);
+    	leftDrive3=new Spark(2);
+    	rightDrive1=new Spark(3);
+    	rightDrive2=new Spark(4);
+    	rightDrive3=new Spark(5);
     	
-    	drivetrain= new RobotDrive(leftDrive1, leftDrive2, rightDrive1, rightDrive2 );
+    	drivetrain= new RobotDrive(leftDrive1, leftDrive2, rightDrive1, rightDrive2);
+    	drivetrain2= new RobotDrive(leftDrive3, rightDrive3);
     	
     	compressor = new Compressor();
     	driveshifter=new Solenoid(0);
        
-    	leftEncoder = new Encoder(4, 5, false, Encoder.EncodingType.k4X);
-    	rightEncoder = new Encoder(6, 7, false, Encoder.EncodingType.k4X);
-    	
-    	leftEncoder.setMaxPeriod(.1);
-    	leftEncoder.setMinRate(10);
-    	leftEncoder.setDistancePerPulse(DIST_PER_PULSE);
-    	leftEncoder.setReverseDirection(false);
-    	leftEncoder.setSamplesToAverage(7);
-    	rightEncoder.setMaxPeriod(.1);
-    	rightEncoder.setMinRate(10);
-    	rightEncoder.setDistancePerPulse(DIST_PER_PULSE);
-    	rightEncoder.setReverseDirection(true);
-    	rightEncoder.setSamplesToAverage(7);
+    	//shooterMotor = new Spark(6);
+    	//shooterEncoder = new Encoder(0, 1, false, Encoder.EncodingType.k4X); //where are Channels A and B plugged in?
+    	//shooter = new Shooter(shooterMotor, shooterEncoder);
     	
     	
+//    	leftEncoder = new Encoder(, , false, Encoder.EncodingType.k4X);
+//    	rightEncoder = new Encoder(, , false, Encoder.EncodingType.k4X);
+    	
+//    	leftEncoder.setMaxPeriod(.1);
+//    	leftEncoder.setMinRate(10);
+//    	leftEncoder.setDistancePerPulse(DIST_PER_PULSE);
+//    	leftEncoder.setReverseDirection(false);
+//    	leftEncoder.setSamplesToAverage(7);
+//    	rightEncoder.setMaxPeriod(.1);
+//    	rightEncoder.setMinRate(10);
+//    	rightEncoder.setDistancePerPulse(DIST_PER_PULSE);
+//    	rightEncoder.setReverseDirection(true);
+//    	rightEncoder.setSamplesToAverage(7);
     }
-    
-	/**
-	 * This autonomous (along with the chooser code above) shows how to select between different autonomous modes
-	 * using the dashboard. The sendable chooser code works with the Java SmartDashboard. If you prefer the LabVIEW
-	 * Dashboard, remove all of the chooser code and uncomment the getString line to get the auto name from the text box
-	 * below the Gyro
-	 *
-	 * You can add additional auto modes by adding additional comparisons to the switch structure below with additional strings.
-	 * If using the SendableChooser make sure to add them to the chooser code above as well.
-	 */
-    
     
     public void autonomousInit() {
     	
     }
 
-    /**
-     * This function is called periodically during autonomous
-     */
     public void autonomousPeriodic() {
-    	leftDrive1.set(linearDriveLeft.compute(leftEncoder.getDistance()));
-    	rightDrive1.set(linearDriveRight.compute(rightEncoder.getDistance()));
-    	leftDrive2.set(linearDriveLeft.compute(leftEncoder.getDistance()));
-    	rightDrive2.set(linearDriveRight.compute(rightEncoder.getDistance()));
-    	leftDrive3.set(linearDriveLeft.compute(leftEncoder.getDistance()));
-    	rightDrive3.set(linearDriveRight.compute(rightEncoder.getDistance()));
-    	
-    	
-    	if(Math.abs(linearDriveRight.getTarget()-PID_SETPOINT)<=TARGET_THRESHOLD)
-    	{
-        	rightDrive1.set(0);
-        	rightDrive2.set(0);
-        	rightDrive3.set(0);
-    	}
-    	if(Math.abs(linearDriveLeft.getTarget()-PID_SETPOINT)<=TARGET_THRESHOLD)
-    	{
-    		leftDrive1.set(0);
-        	leftDrive2.set(0);
-        	leftDrive3.set(0);
-    	}
+//    	leftDrive1.set(linearDriveLeft.compute(leftEncoder.getDistance()));
+//    	rightDrive1.set(linearDriveRight.compute(rightEncoder.getDistance()));
+//    	leftDrive2.set(linearDriveLeft.compute(leftEncoder.getDistance()));
+//    	rightDrive2.set(linearDriveRight.compute(rightEncoder.getDistance()));
+//    	leftDrive3.set(linearDriveLeft.compute(leftEncoder.getDistance()));
+//    	rightDrive3.set(linearDriveRight.compute(rightEncoder.getDistance()));
+//    	
+//    	
+//    	if(Math.abs(linearDriveRight.getTarget()-PID_SETPOINT)<=TARGET_THRESHOLD)
+//    	{
+//        	rightDrive1.set(0);
+//        	rightDrive2.set(0);
+//        	rightDrive3.set(0);
+//    	}
+//    	if(Math.abs(linearDriveLeft.getTarget()-PID_SETPOINT)<=TARGET_THRESHOLD)
+//    	{
+//    		leftDrive1.set(0);
+//        	leftDrive2.set(0);
+//        	leftDrive3.set(0);
+//    	}
     }
 
-    /**
-     * This function is called periodically during operator control
-     */
     public void teleopPeriodic() {
     	drivetrain.arcadeDrive(-xbox1.getRawAxis(1), -xbox1.getRawAxis(4));
+    	drivetrain2.arcadeDrive(-xbox1.getRawAxis(1), -xbox1.getRawAxis(4));
     	if(xbox1.getRawButton(5)){
     		high=false;
     	}
@@ -133,6 +129,14 @@ public class Robot extends IterativeRobot {
     	}
     	
     	driveshifter.set(high);
+    	
+    	
+    	//SHOOTER SECTION
+    	//if(xbox1.getRawButton(0))
+    		//shooterMotor.set(1.0);
+    	
+//    	if(xbox1.getRawButton(1))
+//    		shooter.shootPID();
         
     }
     
--- src/org/usfirst/frc/team2850/robot/Shooter.java
+++ src/org/usfirst/frc/team2850/robot/Shooter.java
@@ -1,5 +1,41 @@
 package org.usfirst.frc.team2850.robot;
 
-public class Shooter {
+import edu.wpi.first.wpilibj.Encoder;
+import edu.wpi.first.wpilibj.Spark;
 
+public class Shooter {
+	private static Spark shooterMotor;
+	private static Encoder shooterEncoder;
+	
+	Robot robot =new Robot();
+	double p = .004;
+	double i = .002;
+	double d = .000;
+	double target = 10; //in feet
+	PID shooterpid = new PID(p,i,d, target, 0);
+	
+	public double instantVelocity;
+	
+	public Shooter(Spark shooterMotor, Encoder shooterEncoder)
+	{
+		Shooter.shooterMotor = shooterMotor;
+		Shooter.shooterEncoder = shooterEncoder;
+		
+		shooterEncoder.setDistancePerPulse(.2); //encoder ticks, gearing to get fps tangential speed
+	}
+	
+	public void shootPID()
+	{
+		shooterMotor.set(shooterpid.compute(shooterEncoder.getRate()));
+		instantVelocity = shooterEncoder.getRate();
+	}
 }
+
+//if(xbox1.getRawButton(0))
+//	motor1.set(1.0);
+//if(maxCurrent<pdp.getCurrent(0))
+//	maxCurrent=pdp.getCurrent(0);
+//sumCurrent += pdp.getCurrent(0);
+//avgCurrent = sumCurrent/iterations;
+//iterations++;
+//System.out.println("Max Current Draw: " + Double.toString(maxCurrent) + "/n Avg Current Draw: " + Double.toString(avgCurrent));
